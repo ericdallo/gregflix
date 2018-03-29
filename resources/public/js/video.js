2739c62256e97b39b4bf7ee89aae1c0c6f429b81
Eric Dallo <ericdallo06@hotmail.com>
2018-03-28 23:11:15 -0300

    Add sound container box

--- resources/public/js/video.js
+++ resources/public/js/video.js
@@ -12,13 +12,19 @@ define(['doc'], function($) {
         videoControls         = $videoControls.first(),
         $playButton           = $videoControls.find('#video-play'),
         $fullScreenButton     = $videoControls.find('#video-fullscreen'),
-        $videoProgress        = $videoControls.find('#video-progress'),
         $videoPlayProgress    = $videoControls.find('#video-play-progress'),
         videoPlayProgress     = $videoPlayProgress.first(),
+        playProgressHalfSize  = (videoPlayProgress.offsetWidth / 2),
         $videoProgressBox     = $videoControls.find('#video-progress-box'),
         videoProgressBox      = $videoProgressBox.first(),
         $videoLength          = $videoControls.find('#video-length'),
         $videoTime            = $videoControls.find('#video-time'),
+        $videoSound           = $videoControls.find('#video-sound'),
+        $videoSoundBox        = $videoControls.find('#video-sound-box'),
+        videoSoundBox         = $videoSoundBox.first(),
+        $videoSoundProgress   = $videoControls.find('#video-sound-progress'),
+        videoSoundProgress    = $videoSoundProgress.first(),
+        soundProgressHalfSize = (videoSoundProgress.offsetWidth / 2),
         isFullscreen          = false,
         playProgressInterval  = 0;
 
@@ -51,6 +57,10 @@ define(['doc'], function($) {
         $videoLength.text(finalLength);
 
         $videoTime.text('00:00');
+
+        var soundBox = $videoControls.find('#video-sound-box').first();
+
+        videoSoundProgress.style.left = soundBox.offsetWidth - soundProgressHalfSize + "px";
     });
 
     var playPause = function() {
@@ -66,6 +76,11 @@ define(['doc'], function($) {
     $videoPlayer.on('click', playPause);
     $playButton.on('click', playPause);
 
+    $videoSound.on('click', function() {
+        videoPlayer.muted = !videoPlayer.muted;
+        $videoSound.toggleClass('mute');
+    });
+
     var trackPlayProgress = function() {
         (function progressTrack() {
             var hours = parseInt(videoPlayer.currentTime / 60 / 60, 10),
@@ -77,7 +92,7 @@ define(['doc'], function($) {
 
             $videoTime.text(currentTime);
 
-            videoPlayProgress.style.left = ( (videoPlayer.currentTime / videoPlayer.duration) * (videoProgressBox.offsetWidth) ) + "px";
+            videoPlayProgress.style.left = ( (videoPlayer.currentTime / videoPlayer.duration) * (videoProgressBox.offsetWidth) - playProgressHalfSize ) + "px";
             playProgressInterval = setTimeout(progressTrack, 50); 
          })(); 
     };
@@ -124,10 +139,10 @@ define(['doc'], function($) {
         isFullscreen ? fullscreenOff() : fullscreenOn();
     });
 
-    var findPosX = function(progressBox) { 
-        var curleft = progressBox.offsetLeft;
-        while( progressBox = progressBox.offsetParent ) {
-            curleft += progressBox.offsetLeft;
+    var findPosX = function(box) { 
+        var curleft = box.offsetLeft;
+        while( box = box.offsetParent ) {
+            curleft += box.offsetLeft;
         } 
         return curleft; 
     }
@@ -137,7 +152,15 @@ define(['doc'], function($) {
 
         var newPercent = Math.max( 0, Math.min(1, (clickX - findPosX(progressBox)) / progressBox.offsetWidth) ); 
         videoPlayer.currentTime = newPercent * videoPlayer.duration; 
-        videoPlayProgress.style.left = newPercent * (progressBox.offsetWidth)  + "px";
+        videoPlayProgress.style.left = newPercent * (progressBox.offsetWidth) - playProgressHalfSize + "px";
+    }
+
+    var setSoundProgress = function(clickX) {
+        var soundBox = $videoControls.find('#video-sound-box').first();
+
+        var newPercent = Math.max( 0, Math.min(1, (clickX - findPosX(soundBox)) / soundBox.offsetWidth) );
+        videoPlayer.volume = newPercent;
+        videoSoundProgress.style.left = newPercent * (soundBox.offsetWidth) - soundProgressHalfSize + "px";
     }
 
     $videoPlayProgress.on('mousedown', function() {
@@ -149,13 +172,24 @@ define(['doc'], function($) {
             setPlayProgress(e.pageX);
         });
 
-        $videoProgressBox.on('mouseup', function(e) {
-            $document.off('mousedown');
+        $document.on('mouseup', function(e) {
             $document.off('mousemove');
+            $document.off('mouseup');
 
             videoPlayer.play();
             setPlayProgress(e.pageX);
             trackPlayProgress();
+        });
+    });
+
+    $videoSoundProgress.on('mousedown', function() {
+        $document.on('mousemove', function(e) {
+            setSoundProgress(e.pageX);
+        });
+
+        $document.on('mouseup', function(e) {
+            $document.off('mousemove');
+            $document.off('mouseup');
         });
     });
 
