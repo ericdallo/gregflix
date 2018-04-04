a8f15de36b6f256c230583ed6c7360010282cf4e
Eric Dallo <ericdallo06@hotmail.com>
2018-04-04 20:37:51 -0300

    fix mysql version of docker container

--- start-db.sh
+++ start-db.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123mudar -d mysql/mysql-server:5.7
+docker run --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123mudar -d mysql/mysql-server:5.6
