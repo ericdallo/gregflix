f339867961e8e8b03bdf14362c7110570ac9af35
Eric Dallo <ericdallo06@hotmail.com>
2018-06-28 21:26:46 -0300

    Improve deploy time

--- configuration/start-app.sh
+++ configuration/start-app.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-docker rm -f gregflix
 docker rmi -f ericdallo/gregflix
+docker pull ericdallo/gregflix
+docker rm -f gregflix
 docker run -d --name gregflix --link mysql:mysql -p 8080:8080  ericdallo/gregflix
