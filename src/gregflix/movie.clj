ad30a6bd99b41094bf9fa57c8574b110eac6a929
Eric Dallo <ericdallo06@hotmail.com>
2018-09-03 21:00:06 -0300

    Add related movies feature

--- src/gregflix/movie.clj
+++ src/gregflix/movie.clj
@@ -2,11 +2,17 @@
   (:use korma.core
   		gregflix.db))
 
-(declare movies)
+(declare movies related-movies)
 
 (defentity movies
   (table :movie)
-  (entity-fields :id :title :slug :description :url))
+  (entity-fields :id :title :slug :description :url)
+  (has-many related-movies))
+
+(defentity related-movies
+  (table :related_movie)
+  (entity-fields :current_movie_id :related_movie_id)
+  (belongs-to movies {:fk :related_movie_id}))
 
 (defn find-all []
 		(select movies))
@@ -16,3 +22,9 @@
 		(select movies
 			(where {:slug slug})
 			(limit 1))))
+
+(defn find-all-related-by [current-movie-id]
+    (select related-movies
+    	(with movies 
+    		(fields :title :slug :url))
+        (where {:current_movie_id current-movie-id})))
