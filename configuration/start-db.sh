3271ba3539d726ef7f43c201fb264d2b69adfb01
Eric Dallo <ericdallo06@hotmail.com>
2018-10-29 19:20:17 -0300

    fix scripts

--- configuration/start-db.sh
+++ configuration/start-db.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 docker run --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123mudar -d mysql/mysql-server:5.6
+sleep 30
+docker exec -it mysql "mysql -u root -p123mudar -e 'create database gregflix;'"
+docker exec -it mysql 'mysql -u root -p123mudar -e "grant all privileges on *.* to 'root'@'%' with grant option;"'
