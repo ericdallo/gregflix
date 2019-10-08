a0a210dd346a85a5d93e4b38976b48fe8330d55b
Eric Dallo <ericdallo06@hotmail.com>
2019-10-08 20:39:23 -0300

    Fix db host

--- src/gregflix/db/config.clj
+++ src/gregflix/db/config.clj
@@ -1,9 +1,12 @@
 (ns gregflix.db.config
   (:require [datomic.api :as d]))
 
-(def datomic-uri (str "datomic:free://localhost:4334/gregflix?password="
-                      (or (System/getenv "DATOMIC_DB_PASSWORD")
-                          "123mudar")))
+(def datomic-uri
+  (let [host     (or (System/getenv "DATOMIC_DB_HOST")
+                     "localhost")
+        password (or (System/getenv "DATOMIC_DB_PASSWORD")
+                     "123mudar")]
+    (str "datomic:free://" host ":4334/gregflix?password=" password)))
 
 (defn datomic-conn []
   (d/connect datomic-uri))
