b7d586a41a872944c42da11a0cdb88ce10361970
Eric Dallo <ericdallo06@hotmail.com>
2019-10-08 10:17:19 -0300

    Migrate start db script to datomic

--- configuration/start-db.sh
+++ configuration/start-db.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-docker run --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=$DATABASE_PASSWORD -d mysql/mysql-server:5.6
-sleep 120
-docker exec -it mysql mysql -u root -p$DATABASE_PASSWORD -e 'create database gregflix;'
-docker exec -it mysql mysql -u root -p$DATABASE_PASSWORD -e "grant all privileges on *.* to 'root'@'%' identified by '$DATABASE_PASSWORD' with grant option;"
+docker run --name datomic -p 4334-4336:4334-4336 -e ALT_HOST=gregflix.site -e ADMIN_PASSWORD=$DATOMIC_ADMIN_PASSWORD -e DATOMIC_PASSWORD=$DATOMIC_DB_PASSWORD -d akiel/datomic-free
