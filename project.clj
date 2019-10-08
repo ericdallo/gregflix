5fc61ebea9880b91552e38feede462bb8cdc17c7
Eric Dallo <ericdallo06@hotmail.com>
2019-10-08 10:17:32 -0300

    Remove korma

--- project.clj
+++ project.clj
@@ -10,27 +10,15 @@
        [com.cemerick/friend "0.2.3"]
        [com.datomic/datomic-free "0.9.5697"]
        [selmer "1.11.6"]
-       [korma "0.4.3"]
-       [mysql/mysql-connector-java "5.1.6"]
-       [migratus "1.0.6"]
        [clj-time "0.15.2"]
-       [migratus-lein "0.5.7"]
        [midje "1.9.9"]]
   :plugins 
       [[lein-ring "0.12.3"]
       [lein-sass "0.5.0"]
-      [lein-midje "3.2.1"]
-      [migratus-lein "0.5.7"]]
+      [lein-midje "3.2.1"]]
   :ring {:handler gregflix.web/app}
   :sass {:src "resources/sass"
          :output-directory "resources/public/css"
          :style :compressed}
   :hooks [leiningen.sass]
-  :migratus {:store :database
-             :migration-dir "migrations"
-             :db {:classname "com.mysql.jdbc.Driver"
-                  :subprotocol "mysql"
-                  :subname "//localhost/gregflix"
-                  :user "root"
-                  :password ""}}
   :main gregflix.main)
