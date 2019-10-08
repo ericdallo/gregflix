9a37f7e73ed26b00d2385bc2f0f7191778cc447c
Eric Dallo <ericdallo06@hotmail.com>
2019-10-08 20:41:11 -0300

    Fix start-app script

--- configuration/start-app.sh
+++ configuration/start-app.sh
@@ -3,4 +3,4 @@
 docker rmi -f ericdallo/gregflix
 docker pull ericdallo/gregflix
 docker rm -f gregflix
-docker run -d --name gregflix --link datomic:datomic -p 8080:8080 --env DATOMIC_DB_PASSWORD=$DATOMIC_DB_PASSWORD ericdallo/gregflix
+docker run -d --name gregflix --link datomic:datomic -p 8080:8080 -e DATOMIC_DB_PASSWORD=$DATOMIC_DB_PASSWORD -e DATOMIC_DB_HOST=$DATOMIC_DB_HOST ericdallo/gregflix
