ac5f01c0d17049303d533dd0a177d18cb01dc8cd
Eric Dallo <ericdallo06@hotmail.com>
2019-11-24 12:31:07 -0300

    #patch add set to start-db

--- configuration/start-db.sh
+++ configuration/start-db.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+set -ve
 
 docker run --name datomic -p 4334-4336:4334-4336 -e ALT_HOST=gregflix.site -e ADMIN_PASSWORD=$DATOMIC_ADMIN_PASSWORD -e DATOMIC_PASSWORD=$DATOMIC_DB_PASSWORD -d akiel/datomic-free
