8baa67049ba608ac6688f4312a7c9f6837b70958
Eric Dallo <ericdallo06@hotmail.com>
2019-11-24 17:18:17 -0300

    Change start script to use github docker image

--- configuration/start-app.sh
+++ configuration/start-app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker rmi -f ericdallo/gregflix
-docker pull ericdallo/gregflix
+docker rmi -f docker.pkg.github.com/ericdallo/gregflix/gregflix
+docker pull docker.pkg.github.com/ericdallo/gregflix/gregflix
 docker rm -f gregflix
-docker run -d --name gregflix --link datomic:datomic -p 8080:8080 -e DATOMIC_DB_PASSWORD=$DATOMIC_DB_PASSWORD -e DATOMIC_DB_HOST=$DATOMIC_DB_HOST ericdallo/gregflix
+docker run -d --name gregflix --link datomic:datomic -p 8080:8080 -e DATOMIC_DB_PASSWORD=$DATOMIC_DB_PASSWORD -e DATOMIC_DB_HOST=$DATOMIC_DB_HOST docker.pkg.github.com/ericdallo/gregflix/gregflix
